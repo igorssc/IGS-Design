f1b9ab1349201c89372b25cf235a21853d028f7d
Igor Santos <igorsantoscosta@gmail.com>
2022-09-20 23:08:26 -0300

    update meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import NextDocument, {
   Main,
   NextScript,
 } from "next/document";
+import coverImg from "../../public/cover.jpg";
 
 class Document extends NextDocument {
   static async getInitialProps(context: DocumentContext) {
@@ -13,11 +14,44 @@ class Document extends NextDocument {
   }
 
   render() {
+    const pageTitle = "IGS Design";
+    const pageImage = coverImg.src;
+    const description =
+      "A IGS Design oferece vários serviços como criação de logotipos, cartões de visita, panfletos, banners, flyers, e convites. Oferecemos também serviços para plataformas online, como criação de sites, blogs e muito mais.";
+
     return (
       <Html lang="pt-br">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="theme-color" content="#199ada"></meta>
+          <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+          <meta name="MobileOptimized" content="320" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="theme-color" content="#199ada" />
+          <meta name="msapplication-TileColor" content="#199ada" />
+          <meta name="referrer" content="no-referrer-when-downgrade" />
+          <meta name="google" content="notranslate" />
+
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={description} />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={pageTitle} />
+          <meta property="og:image" content={pageImage} />
+          <meta property="og:image:secure_url" content={pageImage} />
+          <meta property="og:image:alt" content="Thumbnail" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:title" content={pageTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@rocketseat" />
+          <meta name="twitter:creator" content="@rocketseat" />
+          <meta name="twitter:image" content={pageImage} />
+          <meta name="twitter:image:src" content={pageImage} />
+          <meta name="twitter:image:alt" content="Thumbnail" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
+
           <link rel="icon" href="./favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
