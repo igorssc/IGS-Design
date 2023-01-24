7302e9b9ba54828290c5f11e49bd3282e2915528
Igor Santos <igorsantoscosta@gmail.com>
2023-01-24 19:30:02 -0300

    implementation google analytics #2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,27 +8,27 @@ import "@/styles/globals.scss";
 import PageNProgress from "next-styled-nprogress";
 import type { AppProps } from "next/app";
 import { SnackbarProvider } from "notistack";
-import { Analytics } from "react-analytics";
+import ReactGA from "react-ga";
 
 export default function App({ Component, pageProps }: AppProps) {
+  ReactGA.initialize("G-D4MB103KR2");
+
   return (
-    <Analytics id="G-D4MB103KR2">
-      <SnackbarProvider maxSnack={3}>
-        <BackdropProvider>
-          <PageNProgress
-            color="#fff"
-            showSpinner={false}
-            height="4px"
-            delay={200}
-          />
-          <Navbar />
-          <Component {...pageProps} />
-          <AdditionalInformation />
-          <ButtonWhatsApp />
-          <ButtonScrollTop />
-          <Footer />
-        </BackdropProvider>
-      </SnackbarProvider>
-    </Analytics>
+    <SnackbarProvider maxSnack={3}>
+      <BackdropProvider>
+        <PageNProgress
+          color="#fff"
+          showSpinner={false}
+          height="4px"
+          delay={200}
+        />
+        <Navbar />
+        <Component {...pageProps} />
+        <AdditionalInformation />
+        <ButtonWhatsApp />
+        <ButtonScrollTop />
+        <Footer />
+      </BackdropProvider>
+    </SnackbarProvider>
   );
 }
