d9cce63cdf5d8edb31b36779623d0707e043f42f
Igor Santos <igorsantoscosta@gmail.com>
2023-01-24 19:08:00 -0300

    Add sitemap and keywords

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
   pageImage =
     "https://user-images.githubusercontent.com/26682297/191368584-c36a95f6-f4d1-428c-8014-c22f89fae56a.jpg";
   description =
-    "A IGS Design oferece vários serviços como criação de logotipos, cartões de visita, panfletos, banners, flyers, e convites. Oferecemos também serviços para plataformas online, como criação de sites, blogs e muito mais.";
+    "A IGS Design é uma empresa localizada em Santa Luzia - MG, especializada em desenvolvimento de sites e design gráfico personalizado. Oferecemos soluções de última geração e tendências de mercado, incluindo lojas virtuais e integrações com redes sociais e Google Maps, para garantir que sua empresa esteja sempre na vanguarda. Nossa equipe altamente qualificada trabalha incansavelmente para garantir que o site dos seus sonhos se torne realidade.";
 
   render() {
     return (
@@ -47,6 +47,11 @@ class MyDocument extends Document {
           <meta name="twitter:image:alt" content="Thumbnail" />
           <meta name="twitter:image:width" content="1200" />
           <meta name="twitter:image:height" content="620" />
+
+          <meta
+            name="keywords"
+            content="Desenvolvimento de sites, Design gráfico, Lojas virtuais, Integrações sociais, Google Maps, Revisões ilimitadas, Personalização, Qualidade, Sucesso, Destaque, Inovação, Tecnologia, Empresa local, Santa Luzia, Minas Gerais, Desenvolvimento web, Criação de logotipos, Cartões de visita, Panfletos, Banners, Flyers, Convites, Gráficos, Impressos, Design personalizado, Atendimento ao cliente, Marketing, Comunicação visual, Identidade visual, Criação de marca, Web design, E-commerce, Loja virtual, SEO, Otimização para mecanismos de busca, Integrações, Aplicativos, Redes sociais, Conteúdo, Gerenciamento de conteúdo, Dashboards, Análise de dados, Design responsivo, Mobile, HTML, CSS, JavaScript, PHP, Programação, Banco de dados, Hospedagem, E-mail personalizado, Suporte, Manutenção, Atualizações, Backup, Segurança, Cópia de segurança, Cloud, Armazenamento, Recursos extras, Comunicação, Transparência, Honestidade, Ética, Profissionalismo, Experiência, Conhecimento, Habilidade, Criatividade, Inovação, Flexibilidade, Escalabilidade, escalável, Eficiência, Eficácia, Qualidade, Garantia, Confiança, Referências, Testemunhos, Estudos de caso, Casos de sucesso, Portfólio, Trabalhos anteriores, Parcerias, Colaborações, Desenvolvimento de projetos, Projetos personalizados, Projetos únicos, Projetos criativos, Projetos inovadores, Projetos de sucesso, Projetos de destaque, Projetos de última geração, Projetos de tendência, Projetos de qualidade, Projetos de garantia, Projetos de confiança, Projetos de resultado"
+          />
 
           <link rel="icon" href="./favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
